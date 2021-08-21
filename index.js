545b95be81150a7e5747f48b48a55c4afa09ec78
MIna-Maher <m.maher.eng2014@gmail.com>
2021-08-21 18:03:44 +0200

    Adding files

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+//the @actions/core package provides an interface to the commands in the workflow and helps with input and output variables
+const github = require('@actions/github');
+// the @actions/github package returns the Octokit REST client and GitHub actions contexts
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
